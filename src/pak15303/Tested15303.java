2c7f80818615674e3ace53b70b11ebaeb26bfbc4
Your Name <you@example.com>
2018-11-08 18:49:55 -0500

    1st

---
+++ src/pak15303/Tested15303.java
@@ -0,0 +1,38 @@
+package pak15303;
+
+public class Tested15303 {
+
+	
+	public int returnSum(int[] intarr) 
+	{
+		
+		
+		return 0;
+	}
+	
+	public int returnMax(int [] intarr)
+	{
+		
+		
+		return 0;
+	}
+	
+	
+	
+	public int returnMin(int[] intarr) 
+	{
+		
+		
+		return 0;
+	}
+	
+	public int returnRange(int[] intarr) 
+	{
+		
+		
+		return 0;
+	}
+	
+	
+	
+}
